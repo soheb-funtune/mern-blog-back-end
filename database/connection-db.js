19c533aa896b544089f4e0c957a4d19b03ac109b
soheb-funtune <soheb.sayyad@fyntune.com>
2023-08-11 11:21:57 -0700

    using environment variables

--- database/connection-db.js
+++ database/connection-db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const Connection = async () => {
   await mongoose
-    .connect(
-      `mongodb+srv://sohebs5050:sohebs5050@cluster0.wdmot5y.mongodb.net/blog2?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log("Connected to MongoDB");
     })
